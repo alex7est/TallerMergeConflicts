8f84aa39ed5d4d767bf474794dc9e889069e41a8
alex7est <66407825+alex7est@users.noreply.github.com>
2024-05-31 20:12:49 -0500

    Modificar Product.js Estudiante1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,10 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenidos a mi tienda soy Ivan</Text>
+        <Text style={{color:"blue"}}>Botella de agua: $1.00</Text>
+        <Text style={{color:"orange"}}>Bolsa de papas fritas: $0.50</Text>
+        <Text style={{color:"red"}}>Refresco en lata: $1.25</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,7 +19,7 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'beige',
       alignItems: 'center',
       justifyContent: 'center',
     },
